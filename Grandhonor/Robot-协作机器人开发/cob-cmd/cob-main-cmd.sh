1b56371a3a8a85a9d0f52f9e5bfd3087e72b4556
elliot53 <1604244855@qq.com>
2023-09-04 18:42:54 +0800

    update

---
+++ Grandhonor/Robot-协作机器人开发/cob-cmd/cob-main-cmd.sh
@@ -0,0 +1,264 @@
+#! /bin/bash 
+
+url="http://192.169.7.32:9999"
+
+declare -A cmd_map
+cmd_map[1]="-h"
+cmd_map[2]="--move-to"
+cmd_map[3]="--drag"
+cmd_map[4]="--set-tool"
+cmd_map[5]="--delete-tool"
+cmd_map[6]="--set-default-tool"
+cmd_map[7]="--set-max-cart-vel"
+cmd_map[8]="--set-max-joint-vel"
+cmd_map[9]="--reset-max-cart-vel"
+cmd_map[10]="--reset-max-joint-vel"
+cmd_map[11]="--save-point"
+cmd_map[12]="--delete-point"
+cmd_map[13]="--get-cur-status"
+cmd_map[14]="--get-tool"
+cmd_map[15]="--get-max-cart-vel"
+cmd_map[16]="--get-max-joint-vel"
+cmd_map[17]="--get-points"
+
+########################################################################################
+function Help()
+{
+    echo "cob-jiasheng-cmd.sh  shell script for cob jiasheng-dev branch "
+    echo -e
+
+    echo "${cmd_map[1]}  output "
+    echo "${cmd_map[2]}  output "
+    echo "${cmd_map[3]}  output "
+    echo "${cmd_map[4]}  output "
+    echo "${cmd_map[5]}  output "
+
+    echo -e
+    echo "${cmd_map[6]}  output "
+    echo "${cmd_map[7]}  output "
+    echo "${cmd_map[8]}  output "
+    echo "${cmd_map[9]}  output "
+    echo "${cmd_map[10]}  output "
+
+    echo -e
+    echo "${cmd_map[11]}  output "
+    echo "${cmd_map[12]}  output "
+    echo "${cmd_map[13]}  output "
+    
+    echo -e
+    echo "${cmd_map[14]}  output "
+    echo "${cmd_map[15]}  output "
+    echo "${cmd_map[16]}  output "
+    echo "${cmd_map[17]}  output "
+    echo ""
+}
+
+function MoveTo()
+{
+    uri="/api/control/moveTo"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function Drag()
+{
+    uri="/api/control/drag"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function SetTool()
+{
+    uri="/api/safety/setTool"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function DeleteTool()
+{
+    uri="/api/safety/deleteTool"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function SetDefaultTool()
+{
+    uri="/api/safety/setDefaultTool"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function SetMaxCartVel()
+{
+    uri="/api/safety/setMaxCartVel"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function SetMaxJointVel()
+{
+    uri="/api/safety/setMaxJointVel"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function ResetMaxCartVel()
+{
+    uri="/api/safety/resetMaxCartVel"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function ResetMaxJointVel()
+{
+    uri="/api/safety/resetMaxJointVel"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function SavePoint()
+{
+    uri="/api/teach/savePoint"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function DeletePoint()
+{
+    uri="/api/teach/deletePoint"
+    if [ -z "$1" ]; then 
+        echo "Error json file "
+    else 
+        curl -X POST --data-binary @$1 ${url}${uri}
+    fi
+    echo -e
+}
+
+function GetCurStatus()
+{
+    uri="/api/common/getCurStatus"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function GetTool()
+{
+    uri="/api/safety/getTool"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function GetMaxCartVel()
+{
+    uri="/api/safety/getMaxCartVel"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function GetMaxJointVel()
+{
+    uri="/api/safety/getMaxJointVel"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+function GetPoints()
+{
+    uri="/api/teach/getPoints"
+
+    curl -X GET ${url}${uri}
+        
+    echo -e
+}
+
+########################################################################################
+
+function main()
+{
+    if [[ $1 == "${cmd_map[1]}" ]]; then 
+        Help
+    elif [[ $1 == "${cmd_map[2]}" ]]; then 
+        MoveTo $2
+    elif [[ $1 == "${cmd_map[3]}" ]]; then 
+        Drag $2
+    elif [[ $1 == "${cmd_map[4]}" ]]; then 
+        SetTool $2
+    elif [[ $1 == "${cmd_map[5]}" ]]; then 
+        DeleteTool $2
+    elif [[ $1 == "${cmd_map[6]}" ]]; then 
+        SetDefaultTool $2
+    elif [[ $1 == "${cmd_map[7]}" ]]; then 
+        SetMaxJointVel $2
+    elif [[ $1 == "${cmd_map[8]}" ]]; then 
+        SetMaxCartVel $2
+    elif [[ $1 == "${cmd_map[9]}" ]]; then 
+        ResetMaxCartVel 
+    elif [[ $1 == "${cmd_map[10]}" ]]; then 
+        ResetMaxJointVel 
+    elif [[ $1 == "${cmd_map[11]}" ]]; then 
+        SavePoint 
+    elif [[ $1 == "${cmd_map[12]}" ]]; then 
+        DeletePoint $2
+    elif [[ $1 == "${cmd_map[13]}" ]]; then 
+        GetCurStatus
+    elif [[ $1 == "${cmd_map[14]}" ]]; then 
+        GetTool 
+    elif [[ $1 == "${cmd_map[15]}" ]]; then 
+        GetMaxCartVel 
+    elif [[ $1 == "${cmd_map[16]}" ]]; then 
+        GetMaxJointVel 
+    elif [[ $1 == "${cmd_map[17]}" ]]; then 
+        GetPoints 
+    else 
+        Help
+    fi
+}
+
+if [[ $# > 0 ]]; then 
+    main $*
+else 
+    echo "Too few arguments..."
+    echo "Try './cob-jiasheng-cmd.sh -h' for more information"
+fi
---
+++ Python/python_1-2_编程技巧.md
@@ -0,0 +1,250 @@
+## 简介
+
++ python 常用的编程技巧笔记
+
+## python 解析JSON文件
+
+在Python中解析JSON文件通常是一个非常常见的任务，你可以使用内置的`json`模块来完成这个任务。以下是一个基本的JSON文件解析示例：
+
+假设你有一个名为`data.json`的JSON文件，内容如下：
+
+```json
+{
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+```
+
+你可以使用以下代码来解析这个JSON文件：
+
+```python
+import json
+
+# 打开JSON文件并读取数据
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+# 访问JSON中的数据
+name = data['name']
+age = data['age']
+city = data['city']
+
+# 打印解析后的数据
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+```
+
+运行这段代码将会读取`data.json`文件并将其解析为Python字典对象。然后你可以像访问字典一样访问其中的数据。
+
+如果你要解析的JSON文件包含一个JSON数组，你可以使用`json.load()`来解析整个数组，然后在Python中进一步处理。例如：
+
+假设你有一个名为`data.json`的JSON文件，内容如下：
+
+```json
+[
+    {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    },
+    {
+        "name": "Alice",
+        "age": 25,
+        "city": "Los Angeles"
+    }
+]
+```
+
+你可以使用以下代码来解析包含多个对象的JSON数组：
+
+```python
+import json
+
+# 打开JSON文件并读取数据
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+# 遍历JSON数组中的对象
+for person in data:
+    name = person['name']
+    age = person['age']
+    city = person['city']
+    
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"City: {city}")
+```
+
+这将会遍历JSON数组中的每个对象，并分别访问它们的属性。
+
+## python3 发送网络请求
+
+在Python 3中，你可以使用标准库中的`requests`模块来发送网络请求。以下是一些示例，展示如何发送不同类型的网络请求：
+
+**1. 发送HTTP GET请求：**
+
+```python
+import requests
+
+# 发送GET请求
+response = requests.get('https://www.example.com')
+
+# 检查响应状态码
+if response.status_code == 200:
+    print('请求成功')
+    print(response.text)  # 打印响应内容
+else:
+    print(f'请求失败，状态码：{response.status_code}')
+```
+
+上述代码发送一个HTTP GET请求到`https://www.example.com`，并打印响应内容。
+
+**2. 发送HTTP POST请求：**
+
+```python
+import requests
+
+# POST请求的数据
+data = {'key1': 'value1', 'key2': 'value2'}
+
+# 发送POST请求
+response = requests.post('https://www.example.com/api', data=data)
+
+# 检查响应状态码
+if response.status_code == 200:
+    print('请求成功')
+    print(response.text)  # 打印响应内容
+else:
+    print(f'请求失败，状态码：{response.status_code}')
+```
+
+上述代码发送一个HTTP POST请求到`https://www.example.com/api`，并附带数据作为表单数据发送。
+
+**3. 发送HTTP请求并处理JSON响应：**
+
+```python
+import requests
+
+# 发送GET请求
+response = requests.get('https://api.example.com/data')
+
+# 检查响应状态码
+if response.status_code == 200:
+    print('请求成功')
+    json_data = response.json()  # 将JSON响应解析为Python字典或列表
+    print(json_data)
+else:
+    print(f'请求失败，状态码：{response.status_code}')
+```
+
+上述代码发送一个HTTP GET请求，然后使用`.json()`方法将JSON响应解析为Python数据结构。
+
+以上是使用`requests`库发送HTTP请求的基本示例。在实际应用中，你还可以添加请求头、处理异常、处理重定向、使用认证等高级功能，具体取决于你的需求。确保在使用`requests`模块时，你已经安装了它，你可以使用`pip`来安装它：
+
+```
+pip install requests
+```
+
+## python3 通过POST请求发送JSON数据
+
+你可以使用Python 3中的`requests`库发送POST请求并附加JSON数据。以下是一个示例，展示如何发送带有JSON数据的POST请求：
+
+```python
+import requests
+import json
+
+# 定义要发送的JSON数据
+data = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# 将JSON数据转换为字符串
+json_data = json.dumps(data)
+
+# 设置请求头，指明发送的数据是JSON格式
+headers = {'Content-Type': 'application/json'}
+
+# 发送POST请求
+response = requests.post('https://www.example.com/api', data=json_data, headers=headers)
+
+# 检查响应状态码
+if response.status_code == 200:
+    print('请求成功')
+    print(response.text)  # 打印响应内容
+else:
+    print(f'请求失败，状态码：{response.status_code}')
+```
+
+上述代码首先将Python字典转换为JSON格式的字符串，然后设置请求头以指明发送的数据是JSON格式。接下来，它使用`requests.post`方法发送POST请求到`https://www.example.com/api`，并附带JSON数据作为请求正文。
+
+确保替换URL为你要发送请求的目标URL，以及替换`data`变量中的JSON数据为你要发送的实际数据。这样，你就可以通过POST请求发送JSON数据了。
+
+## python3 解析JSON 判断字段类型 遍历数组
+
+要解析JSON并根据字段类型进行判断以及遍历JSON数组，你可以使用Python 3的`json`模块来解析JSON数据，然后根据需要进行类型检查和遍历。以下是一个示例：
+
+假设你有一个JSON文件`data.json`，其中包含一个JSON数组，如下所示：
+
+```json
+[
+    {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    },
+    {
+        "name": "Alice",
+        "age": null,
+        "city": "Los Angeles"
+    },
+    {
+        "name": "Bob",
+        "age": "25",
+        "city": "Chicago"
+    }
+]
+```
+
+你可以使用以下Python 3代码来解析JSON，检查字段类型并遍历数组：
+
+```python
+import json
+
+# 打开JSON文件并读取数据
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+# 遍历JSON数组中的对象
+for person in data:
+    name = person['name']
+    age = person['age']
+    city = person['city']
+
+    # 检查字段类型
+    if isinstance(age, int):
+        age_str = str(age)  # 将年龄转换为字符串
+        age_type = 'int'
+    elif age is None:
+        age_str = 'N/A'  # 如果年龄字段为null，则将其标记为N/A
+        age_type = 'null'
+    elif isinstance(age, str):
+        age_str = age
+        age_type = 'string'
+    else:
+        age_str = 'Unknown'
+        age_type = 'unknown'
+
+    # 打印信息
+    print(f"Name: {name}")
+    print(f"Age: {age_str} (Type: {age_type})")
+    print(f"City: {city}")
+    print()
+```
+
+上述代码会遍历JSON数组中的每个对象，并检查年龄字段的类型。根据字段类型，它会打印相应的信息。
+
+这个示例中，我们假设年龄字段可能是整数、字符串、null或其他未知类型，然后根据实际类型执行相应的操作。你可以根据自己的JSON数据结构和字段类型进行适当的调整。
